29e8c86daa3737841698cc16650508a83c9d141c
valceven <valmykelcevenbolante@gmail.com>
2024-11-26 23:21:27 +0800

    added basic lang routings plus folder structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import LandingPage from './pages/LandingPage';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import ErrorPage  from './pages/ErrorPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
