dbb3b84fddcf91407b1fec690fb3131cbb7e127c
markyleangela <markyleangela@gmail.com>
2024-11-26 22:53:09 +0800

    Change landing page ui

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,13 +6,18 @@ import Logo from '../assets/Logo.png';
 const LandingPage = () => {
   return (
     <div className="landing-page">
+      <nav className='navbar'>
+          <img src={Logo} alt="Logo" />
+        </nav>
       <header>
-      <img src={Logo} alt="Logo" />
-        <h3>Streamline Your <br/> Barangay <br/> Transactions<br/> with LookAt</h3>
-        <p>Experience fast, simple, and organized document processing. From applications to appointments, LookAt keeps everything at your fingertips.</p>
+        
+     
+        
       </header>
       <main>
         <section className="cta">
+        <h3 className='statement-1'>Streamline Your <br/> Barangay <br/> Transactions<br/> with LookAt</h3>
+        <p className='statement-2'>Experience fast, simple, and organized document processing. From applications to appointments, LookAt keeps everything at your fingertips.</p>
           <button className='get-started-btn'>Get Started</button>
           <p>Already have an account? <a href='' className='login-a'>Login</a></p>
         </section>
