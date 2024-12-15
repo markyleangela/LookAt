ac16e1148a4116f42de4aecd8a7c692411688bec
MKdelosreyes <151009128+MKdelosreyes@users.noreply.github.com>
2024-12-15 20:29:08 +0800

    added profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar'
 import '../styles/Profile.css'
 import { Link, useNavigate } from 'react-router-dom';
 import UserApi from '../api/userApi';
-import '../styles/Profile.css'
 
 const Profile = () => {
   const [firstName, setFirstName] = useState('');
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import '../styles/Signup.css';
 import PersonGear from '../assets/person-gear.png';
-import ArrowButton from '../assets/back-arrow.png';
-import { Link } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import { useNavigate } from 'react-router-dom';
 import UserApi from '../api/userApi'; // Import the registerUser function
-import {sendVerificationCode} from '../components/SendVerification'
+import { Link } from 'react-router-dom';
 
-
-
-const Signup  = () =>  {
+const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -32,36 +27,24 @@ const Signup  = () =>  {
             return;
         }
 
+        const userData = {
+            "email": email,
+            "password": password
+        };
   
 
-        const userData = {
-                    userDto: {
-                        userId: 0,
-                        userName: "string",
-                        firstName: "string",
-                        lastName: "string",
-                        password: password,
-                        mobileNumber: "string",
-                        date: "2024-11-30T13:11:38.086Z",
-                        physicalIdNumber: "string",
-                        purok: "string",
-                        barangayLoc: "string",
-                        cityMunicipality: "string",
-                        province: "string",
-                        email: email,
-                        isVerified: false,
-                        barangayId: "string"
-                    },
-                    "otp": 0
-        }
 
         try {
             // Use registerUser from UserApi
             const response = await UserApi(userData);  // Call the function to register the user
-            
-            if (response.status === 201) {
-
-                navigate('/account-created');
+            console.log(response.status)
+            if (response.status === 200) {
+                // Pass the email and password to the email verification page
+                console.log("Signup - Email:", email);
+                console.log("Signup - Password:", password);
+                navigate('/email-verification', {
+                    state: { email, password }
+                });
             }
         } catch (error) {
             console.error(error);
@@ -72,41 +55,35 @@ const Signup  = () =>  {
     return (
         <div className='login'>
             <header className='back-arrow'>
-                <BackButton/>
+                <BackButton />
             </header>
             <section className='content'>
-                
                 <img src={PersonGear} alt="Icon" />
                 <h1 className='font-sans text-xl'>SIGN UP</h1>
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
                 <form onSubmit={handleSubmit}>
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <label>Confirm Password</label>
-                        <input
-                            type="password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                        />
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <label>Confirm Password</label>
+                    <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
                     <button className='login-btn' type='submit'>Sign Up</button>
                     <p className='font-sans text-xl'>Already have an account? <Link to='/login' className='login-a'>Log in</Link></p>
                 </form>
-              
-                
-            
             </section>
-            
-
         </div>
     );
 };
